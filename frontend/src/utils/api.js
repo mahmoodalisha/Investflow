29aa6cd5015d20bef5337965538e0be49f4bfed7
Alisha <mahmoodalisha35@gmail.com>
2026-06-13 15:31:13 +0530

    deploying frontend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Make sure this matches your backend port!
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api',
 });
 
 api.interceptors.request.use(
